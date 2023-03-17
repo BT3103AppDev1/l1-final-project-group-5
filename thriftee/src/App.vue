bad34a303b5f584d9fca153c2bad1970256f7060
Rena Ng <e0772915@u.nus.edu>
2023-03-17 17:34:36 +0800

    change

--- thriftee/src/App.vue
+++ thriftee/src/App.vue
@@ -33,15 +33,6 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
+} 
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
