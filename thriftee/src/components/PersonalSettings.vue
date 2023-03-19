464f27cfddb79bcf3719012a05c07e963d3a9a68
giffordlee <giffordlee20@gmail.com>
2023-03-19 21:42:35 +0800

    created CreateListing component

--- thriftee/src/components/PersonalSettings.vue
+++ thriftee/src/components/PersonalSettings.vue
@@ -248,7 +248,6 @@ input[type=text], select {
   margin: 8px 0;
   display: block;
   border: 1px solid #ccc;
-  border-radius: 4px;
   box-sizing: border-box;
   height: 40px;
   border-radius: 10px;
