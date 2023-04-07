4baa7b0cf10c2e201d8e1c8b7c1cf6f4cc4f1542
yjtang7 <97512390+yjtang7@users.noreply.github.com>
2023-04-07 15:30:30 +0800

    router for each listing based on id

---
+++ thriftee/src/views/ViewListing.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        Able to access Data
+        {{this.currentListing}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ViewListing",
+    data() {
+        return {
+            currentListing: null,
+        }
+    },
+    async created() {
+        this.currentListing = await this.$store.state.listingArray.filter((listing) => {
+            return listing.ListingID === this.$route.params.listingid;
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
